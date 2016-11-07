3b9815779295db7cf180dce182e452d54cd90031
zhongzhi107 <zhong.zhi@163.com>
2016-11-07 22:59:11 +0800

    v1.0.1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var url = require('url');
 var util = require('util');
-var assign = require('object-assign');
+var assign = require('object-assign-deep');
 var clearRequire = require('clear-require');
 var ejs = require('ejs');
 
@@ -12,6 +12,7 @@ module.exports = function(options) {
     extension: '.ejs',
     templates: '.',
     mockData: '.',
+    globalData: '__global.js',
     rewriteRules: {}
   }, options);
   return function(req, res, next) {
@@ -19,24 +20,35 @@ module.exports = function(options) {
     var pathname = options.rewriteRules[urlObject.pathname] || urlObject.pathname;
     var templateAbsPath = path.resolve(path.join(options.templates, pathname));
     var dataAbsPath = path.resolve(path.join(options.mockData, pathname.replace(options.extension, '.js')));
+    var globalDataPath = path.resolve(path.join(options.mockData, options.globalData));
     if (fs.existsSync(templateAbsPath)) {
       var tpl = fs.readFileSync(templateAbsPath, {encoding: options.encoding});
-      var context = {};
-      if (fs.existsSync(dataAbsPath)) {
-        try {
-          var contextExport = require(dataAbsPath);
-          if (util.isFunction(contextExport)) {
-            context = contextExport(req, res);
-          } else {
-            context = contextExport;
-          }
-        }
-        catch (e) {
-          console.log('File "' + dataAbsPath + ' require failed.\n' + e);
+      if (fs.existsSync(templateAbsPath)) {
+      var globalContext = {};
+      if (fs.existsSync(globalDataPath)) {
+        var gcontext = require(globalDataPath);
+        if (util.isFunction(gcontext)) {
+          globalContext = gcontext(req, res);
+        } else {
+          globalContext = gcontext;
         }
       }
-      var output = ejs.render(tpl, context);
-      res.end(output);
+      var pageContext = {};
+      if (fs.existsSync(dataAbsPath)) {
+        var pcontext = require(dataAbsPath);
+        if (util.isFunction(pcontext)) {
+          pageContext = pcontext(req, res);
+        } else {
+          pageContext = pcontext;
+        }
+      }
+      try {
+        var output = ejs.render(tpl, assign(globalContext, pageContext));
+        res.end(output);
+      } catch (e) {
+        console.log(e);
+        next();
+      }
     } else {
       next();
     }
